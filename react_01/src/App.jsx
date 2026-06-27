23d24d2940781378b2892cc7671e0999684fa981
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-06-27 15:07:51 +0500

    add first react project

---
+++ react_01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div  id = "parent">
+      <h1 id = "child_1">An individual comes into world alone and leaves it alone. </h1>
+      <br></br>
+      <h2 id = "child_2" >World is MORTAL</h2>
+    </div>
+  )
+}
+
+export default App
