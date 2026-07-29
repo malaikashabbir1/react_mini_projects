20010d5114cbcb73da1d742a45b7c078dac8ace2
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-07-29 14:42:22 +0500

    Set up React project with Tailwind CSS

---
+++ 06_tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 text-white '>
+      <button className=' bg-yellow-300 text-black' >Tailwind Setup Here! </button>
+    </div>
+  )
+}
+
+export default App
