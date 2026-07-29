d86d2d53ba959a6f13740a6f731d60919338d171
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-07-29 14:39:09 +0500

    Add initial CSS styling to React project

---
+++ css_05/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles  from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className= {styles.btn}>
+        <h3 >Click Here! </h3>
+    </div>
+  )
+}
+
+export default Button
